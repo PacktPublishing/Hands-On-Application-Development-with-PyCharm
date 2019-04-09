8c92ab7d3e3d06fe6d4c2397e01e722abecb6680
KrisNguyen135 <nguyenminhquan135@gmail.com>
2019-04-09 17:36:18 -0400

    finished intro for chapter 7

---
+++ Chapter07/Intro/script.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHi() {
+        alert("Hello, I'm " + this.name)
+    }
+}
+
+let p = new Person("Quan");
+//p.sayHi();
+document.body.innerHTML = "<p>Hello, I'm " + p.name + "</p>"
