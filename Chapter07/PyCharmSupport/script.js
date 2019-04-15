6aeea3a42eaf1e55424133fcad69d6fa4d00e27a
KrisNguyen135 <nguyenminhquan135@gmail.com>
2019-04-15 12:12:24 -0400

    finished chapter 7

---
+++ Chapter07/PyCharmSupport/script.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHi() {
+        alert("Hello, I'm " + this.name)
+    }
+}
+
+//let p = new Person("Quan");
+//p.sayHi()
