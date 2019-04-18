b05c3e066e0e6a1992985a36a82630caadd1585e
KrisNguyen135 <nguyenminhquan135@gmail.com>
2019-04-18 15:30:36 -0400

    ignored venv files in chapter 8

--- Chapter08/mysite/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/quannguyen/PycharmProjects/PyCharm-Book/Chapter08/mysite/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
