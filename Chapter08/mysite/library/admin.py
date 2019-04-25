20077413fb310fd8ac5b502e98ee417bcbc24028
KrisNguyen135 <nguyenminhquan135@gmail.com>
2019-04-25 15:54:21 -0400

    finished django tutorial for chapter 8

--- Chapter08/mysite/library/admin.py
+++ Chapter08/mysite/library/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book
+
+
+class BookInLine(admin.TabularInline):
+    model = Book
+    extra = 1
+
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInLine]
+
+
+admin.site.register(Author, AuthorAdmin)
--- Chapter08/mysite/library/views.py
+++ Chapter08/mysite/library/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
+def index(request):
+    latest_books = Book.objects.order_by('-pub_date')[:5]
+    context = {'latest_books': latest_books}
+    return render(request, 'library/index.html', context)
