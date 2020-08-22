991557ca1042ec091785ea3f21b249cc3b9f75f1
Quan Nguyen <nguyenminhquan135@gmail.com>
2020-08-22 13:31:23 -0500

    Update test_example.py

--- Chapter06/Testing/test_example.py
+++ Chapter06/Testing/test_example.py
@@ -8,9 +8,6 @@ class MathTest(TestCase):
     def test_mul(self):
         self.assertEqual(2 * 5, 10)
 
-    def test_exp(self):
-        self.assertEqual(2 ** 3, 9)
-
 
 class StringTest(TestCase):
     def test_stringcase(self):
